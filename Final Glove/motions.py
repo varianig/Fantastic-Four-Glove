a5b52e5d294e6e51b532f770b24a8cbfcdea1fc0
varianig <165704818+varianig@users.noreply.github.com>
2026-04-25 16:53:54 -0400

    Update motions.py

--- Final Glove/motions.py
+++ Final Glove/motions.py
@@ -1,31 +1,35 @@
-from main import FORCE_INDEX, FORCE_MIDDLE, FORCE_THUMB, FLEX_INDEX, FLEX_MIDDLE, FLEX_THUMB, FORCE_RING
+# "FLEX_INDEX", "FLEX_MIDDLE"
 
 motions = {
-    "left click": [FORCE_THUMB],
-    "right click": [FLEX_MIDDLE],
-    "tab": [FORCE_INDEX],
-    "shift + tab": [FORCE_MIDDLE],
-    "enter": [FORCE_MIDDLE, FORCE_THUMB],
-    "esc": [FORCE_INDEX, FORCE_THUMB],
-    "pg dwn": [FLEX_INDEX, FLEX_THUMB],
-    "pg up": [FLEX_INDEX],
-    "left arrow": [FLEX_INDEX, FLEX_THUMB],
-    "right arrow": [FLEX_MIDDLE, FLEX_THUMB],
+    "left click": ["FORCE_THUMB"],
+    "right click": ["FLEX_THUMB"],
+    "tab": ["FORCE_INDEX"],
+    "shift + tab": ["FORCE_MIDDLE"],
+    "enter": ["FORCE_MIDDLE", "FORCE_THUMB"],
+    "esc": ["FORCE_INDEX", "FORCE_THUMB"],
+    "pg dwn": ["FLEX_INDEX"],
+    "pg up": ["FLEX_MIDDLE"],
+    "left arrow": ["FLEX_INDEX", "FLEX_THUMB"],
+    "right arrow": ["FLEX_MIDDLE", "FLEX_THUMB"],
 
-    "toggle cursor": [FORCE_RING],
-    "toggle keyboard": [FLEX_THUMB] # WIN + CMD + O on windows
+    "toggle cursor": ["FORCE_RING"],
+    "toggle keyboard": ["FORCE_INDEX", "FORCE_MIDDLE"] # WIN + CMD + O on windows
 }
 
 motions_cursor = {
-    "mouse yneg": [FORCE_INDEX],
-    "mouse ypos": [FORCE_MIDDLE],
-    "mouse xneg": [FORCE_THUMB],
-    "mouse xpos": [FLEX_THUMB],
+    "left click": ["FORCE_THUMB"],
+    "right click": ["FLEX_THUMB"],
 
-    "mouse xneg yneg": [FORCE_INDEX, FORCE_THUMB],
-    "mouse xneg ypos": [FORCE_MIDDLE, FORCE_THUMB],
-    "mouse xpos yneg": [FLEX_THUMB, FORCE_INDEX],
-    "mouse xpos ypos": [FLEX_THUMB, FORCE_MIDDLE],
+    "mouse yneg": ["FORCE_INDEX"],
+    "mouse ypos": ["FLEX_INDEX"],
+    "mouse xneg": ["FORCE_MIDDLE"],
+    "mouse xpos": ["FLEX_MIDDLE"],
 
-    "toggle cursor": [FORCE_RING]
+    "mouse xneg yneg": ["FORCE_INDEX", "FORCE_MIDDLE"],
+    "mouse xneg ypos": ["FLEX_INDEX", "FORCE_MIDDLE"],
+    "mouse xpos yneg": ["FORCE_INDEX", "FLEX_MIDDLE"],
+    "mouse xpos ypos": ["FLEX_INDEX", "FLEX_MIDDLE"],
+
+    "toggle cursor": ["FORCE_RING"],
+    "toggle keyboard": ["FORCE_INDEX", "FORCE_MIDDLE"] # WIN + CMD + O on windows
 }
