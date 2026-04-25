316f01d3c2c11947eee11cfaa89025d141d2ed32
varianig <165704818+varianig@users.noreply.github.com>
2026-04-25 16:54:16 -0400

    Update main.py

--- Final Glove/main.py
+++ Final Glove/main.py
@@ -9,24 +9,26 @@ FLEX_INDEX = analogio.AnalogIn(board.GP26)
 FLEX_MIDDLE = analogio.AnalogIn(board.GP26)
 FLEX_THUMB = analogio.AnalogIn(board.GP26)
 FORCE_RING = analogio.AnalogIn(board.GP26)
-force = [FORCE_INDEX, FORCE_MIDDLE, FORCE_THUMB, FORCE_RING]
-flex = [FLEX_INDEX, FLEX_MIDDLE, FLEX_THUMB]
+force = ["FORCE_INDEX", "FORCE_MIDDLE", "FORCE_THUMB", "FORCE_RING"]
+flex = ["FLEX_INDEX", "FLEX_MIDDLE", "FLEX_THUMB"]
 
-glove = Glove()
+glove = Glove(FORCE_INDEX, FORCE_MIDDLE, FORCE_THUMB, FLEX_INDEX, FLEX_MIDDLE, FLEX_THUMB, FORCE_RING)
 
 # Calibration
 print("Calibrating...")
 # Glove.calibrate() returns dict of thresholds, order is forceI, forceM, forceP, flecI, flexM, flexP, forceA
 thresh = glove.calibrate()
 print("Calibration Complete.\nData:")
-print("foI", thresh[0], "\nfoM", thresh[1], "\nfoP", thresh[2], "\nflI", thresh[3], "\nflM", thresh[4], "\nflP", thresh[5], "\nfoA", thresh[6])
+print("foI", thresh["FORCE_INDEX"], "\nfoM", thresh["FORCE_MIDDLE"], "\nfoP", thresh["FORCE_THUMB"],
+       "\nflI", thresh["FLEX_INDEX"], "\nflM", thresh["FLEX_MIDDLE"], "\nflP", thresh["FLEX_THUMB"], 
+       "\nfoA", thresh["FORCE_RING"])
 time.sleep(5)
 
 # Main Loop
-mot = []
+mot = set()
 action = False
 while True:
-    vals = glove.readVals(FORCE_INDEX, FORCE_MIDDLE, FORCE_THUMB, FLEX_INDEX, FLEX_MIDDLE, FLEX_THUMB, FORCE_RING)
+    vals = glove.readVals()
     if action == False:
         for val, i in vals.items():
             if val in flex and i < thresh[val] + 100: 
@@ -36,14 +38,17 @@ while True:
 
     if action == True:
         for val, i in vals.items():
-            if val in flex and val < thresh[val] + 250:
+            if val in flex and i < thresh[val] + 250:
                 if val not in mot:
-                    mot.append(val)
-            if val in force and val < thresh[val] + 150:
+                    mot.add(val)
+            if val in force and i < thresh[val] + 150:
                 if val not in mot:
-                    mot.append(val)
-        if all(i < thresh[val] + 100 for i, val in vals):
+                    mot.add(val)
+        if all(value < thresh[key] + 100 for key, value in vals.items()):
             glove.completeAction(mot)
+            action = False
+            mot = []
+            time.sleep(0.3)
         
     time.sleep(0.05)
         
